e648399146b51811dcc6b9969d010ced127ae445
Deformator <dammandrii@gmail.com>
2017-09-16 22:16:02 -0400

    First symbols implementing AC, square, %

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /*    override func didReceiveMemoryWarning() {
+     super.didReceiveMemoryWarning()
+     // Dispose of any resources that can be recreated.
+     }
+     */
+    
+    @IBOutlet weak var screen: UILabel!
+    
+    var userStartTyping : Bool = false
+    
+    var totalResult = 0
+    
+    var firstDidgit : Double = 0
+    var secondDigit : Double = 0
+    
+    @IBAction func pressedButton(_ sender: UIButton) {
+        let title = sender.title(for: .selected)!
+        if userStartTyping {
+            let textOnScreen = screen.text!
+            screen.text = textOnScreen + title
+        } else {
+            screen.text = title
+            userStartTyping = true
+        }
+        
     }
-
-
+    
+    @IBAction func countResult(_ sender: UIButton) {
+        userStartTyping = false
+        let operand = Double(screen.text!)!
+        let pressedSymbol = sender.currentTitle
+        if (pressedSymbol != nil) {
+            switch sender.currentTitle! {
+            case "AC":
+                screen.text = "0"
+                firstDidgit = 0
+                secondDigit = 0
+                userStartTyping = false
+            case "√":
+                let resultOfOperation = sqrt(operand)
+                screen.text = String(resultOfOperation)
+            case "%":
+                let resultOfOperation = operand / 100
+                screen.text = String(resultOfOperation)
+            case "+":
+                firstDidgit = operand
+                userStartTyping = false
+            default:
+                break
+            }
+        }
+    }
+    
+    
 }
 
